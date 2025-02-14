e3e47aa62d0b81863643c4d962549950cecde056
Tierres Griep <tierresgriep@gmail.com>
2025-02-14 16:14:30 -0300

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+export const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-950 mx-auto'>
+        <h1>Hello TailwindCss v4</h1>
+      </div>
+      <div>
+        <h2 className='container bg-amber-800'>
+          Second Tittle
+        </h2>
+      </div>
+    </div>
+  )
+}
