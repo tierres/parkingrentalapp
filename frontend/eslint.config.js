e6aeaf483b4443bdff20619a3a4f6c5bd715ade1
Tierres Griep <tierresgriep@gmail.com>
2025-04-16 20:12:39 -0300

    PC - 16/04/25 - Eslint configurado - Verificar se funcionou

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react'; // ✅ import do plugin react
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -14,15 +15,23 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react, // ✅ adiciona o plugin react
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+);
