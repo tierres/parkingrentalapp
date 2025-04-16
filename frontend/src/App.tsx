467a822b229d87411f44b9706646436423800821
Tierres Griep <tierresgriep@gmail.com>
2025-04-16 19:45:39 -0300

    PC - 16/04/2025 - Organizando os arquivos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Dashboard from './pages/Dashboard/Dashboard'
-import Home from './pages/Home/Home'
+import { Home } from './pages/Home/Home'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <main className="p-4">
+          <h1 className="text-2xl font-bold mb-4">Gerencie seu estacionamento</h1>
+          <p>Desenhe o mapa, marque vagas e acompanhe os aluguéis.</p>
+        </main>
+    );
+}
