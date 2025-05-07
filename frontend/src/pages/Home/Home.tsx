fd9398145caea08212e36198de537479293e38fc
Tierres Griep <tierresgriep@gmail.com>
2025-05-07 17:31:19 -0300

    PC - 07/05/25 - 17h30 - Trabalhando na Home Page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 
 export const Home = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-800 via-blue-600 to-blue-400">
+    <main className="relative z-0 min-h-screen bg-gradient-to-b from-blue-800 via-blue-600 to-blue-400">
       <header className="w-full h-20 flex items-center justify-between px-8">
         <h1 className="text-white text-5xl font-thin drop-shadow-xl tracking-wide">
           parking rental .app
@@ -19,15 +19,26 @@ export const Home = () => {
         </div>
       </header>
 
-      {isMenuOpen && (
-        <div className="absolute right-0 mt-2 w-40 bg-white rounded-xl shadow-lg p-4 z-10">
-          <h2 className="text-xl font-semibold mb-4">Menu</h2>
-          <ul className="space-y-2">
-            <li>Meu Perfil</li>
-            <li>Entre em contato</li>
-          </ul>
+      <div
+        className={`z-50 fixed top-0 right-0 h-full w-64 bg-blue-800 shadow-xl transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="p-4 flex justify-between items-center border-b">
+          <h2 className="text-xl text-white font-semibold">Menu</h2>
+          <X
+            className="w-5 h-5 text-white cursor-pointer"
+            onClick={toggleMenu}
+          />
         </div>
-      )}
+        <ul className="p-4 space-y-4">
+          <li className="text-white cursor-pointer">Meu Perfil</li>
+          <li className="text-white cursor-pointer">Entre em contato</li>
+          <li className="text-white cursor-pointer">Sair</li>
+        </ul>
+      </div>
+
+      {isMenuOpen && <div onClick={toggleMenu} className="fixed inset-0" />}
     </main>
   );
 };
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,9 @@
+export default {
+    theme: {
+      extend: {
+        colors: {
+          luar: "#0052cc",
+        },
+      },
+    },
+  };
